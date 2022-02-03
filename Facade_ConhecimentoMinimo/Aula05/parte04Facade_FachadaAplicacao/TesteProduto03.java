daf940b5b846ed879e06e96a76da11ea7a7e4049
Adison Soares <adison.soares7@gmail.com>
2022-02-02 21:09:40 -0300

    Finalizando Facade, iniciando Singleton

--- Facade_ConhecimentoMinimo/Aula05/parte04Facade_FachadaAplicacao/TesteProduto03.java
+++
@@ -1,8 +0,0 @@
-package Aula05.parte04Facade_FachadaAplicacao;
-
-public class TesteProduto03 {
-	public static void main(String[] args) {
-		Produto produto = new Produto(01,"CD",7.50);
-		System.out.println(produto);
-	}
-}
