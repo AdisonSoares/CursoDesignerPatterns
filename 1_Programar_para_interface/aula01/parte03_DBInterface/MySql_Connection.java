8c65bbe18bb8ad85512ce634850eb01baf396f5d
Adison Soares <adison.soares7@gmail.com>
2022-02-07 11:31:27 -0300

    Aula 01 - Parte 03 - Implementando banco de dados - Iteracao IConection e SqlServer.

--- 1_Programar_para_interface/aula01/parte03_DBInterface/MySql_Connection.java
+++ 1_Programar_para_interface/aula01/parte03_DBInterface/MySql_Connection.java
@@ -1,4 +1,4 @@
-package aula01.parte03_DBInterface_;
+package aula01.parte03_DBInterface;
 
 /**
  * @Classe que simula a conexão
@@ -9,15 +9,15 @@ package aula01.parte03_DBInterface_;
  * @ImplementandoConnection e seus métodos
  * ela se torna objeto da interface, podendo
  * ser melhor manipulada.
- * 
+ *
  * @PrincípioDeFavorecimentoDaComposiçãoSobreHerança
  * Principio de designer simples, outros tipos de designes
  * se baseiam nela para confecção do arranjo entre as classes envolvidas
  * do designer em específico, nesse exemplo se programa para INTERFACE.
  */
-public class Oracle_Connection implements IConnection_ {
+public class MySql_Connection implements IConnection_{
 	@Override
-	public void connect() {
-		System.out.println("Conectando ao Oracle");
+	public void connect(){
+		System.out.println("Conectando ao MySql");
 	}
 }
