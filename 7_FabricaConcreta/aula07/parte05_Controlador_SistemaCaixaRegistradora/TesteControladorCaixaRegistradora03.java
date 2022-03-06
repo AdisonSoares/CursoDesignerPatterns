6a71658171ea5843b16343e12ff8bc9c18089ac0
Adison Soares <adison.soares7@gmail.com>
2022-03-06 20:35:25 -0300

    Aula 07 - Parte 05 - Controlador - Sistema Caixa.

---
+++ 7_FabricaConcreta/aula07/parte05_Controlador_SistemaCaixaRegistradora/TesteControladorCaixaRegistradora03.java
@@ -0,0 +1,47 @@
+package aula07.parte05_Controlador_SistemaCaixaRegistradora;
+
+public class TesteControladorCaixaRegistradora03 {
+	public static void testeSistemaCaixaControlador() {
+	
+		Controlador_CaixaRegistradora controladorSistemaCaixaRegistradora;
+		controladorSistemaCaixaRegistradora = new Controlador_CaixaRegistradora();
+		
+		System.out.println("Testando a integração do sistema estoque ItauTec");
+		controladorSistemaCaixaRegistradora.criacaoAdapterSistemaCaixaRegistradoraEstoque("ItauTec");
+		controladorSistemaCaixaRegistradora.diminuirEstoque();
+		
+		
+		System.out.println("Testando a integração do sistema estoque IBM");
+		controladorSistemaCaixaRegistradora.criacaoAdapterSistemaCaixaRegistradoraEstoque("IBM");
+		controladorSistemaCaixaRegistradora.diminuirEstoque();
+		
+		System.out.println("Testando a integração do sistema contabil ItauTec");
+		controladorSistemaCaixaRegistradora.criacaoAdapterSistemaCaixaRegistradoraContabil("ItauTec");
+		controladorSistemaCaixaRegistradora.registroVendaSistemaContabil();
+		
+		
+		System.out.println("Testando a integração do sistema contabil IBM");
+		controladorSistemaCaixaRegistradora.criacaoAdapterSistemaCaixaRegistradoraContabil("IBM");
+		controladorSistemaCaixaRegistradora.registroVendaSistemaContabil();
+	}
+	
+	public static void main(String[] args) {
+		testeSistemaCaixaControlador();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
